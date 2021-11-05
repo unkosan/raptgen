d0573665f525afa9eeba9bac6de28b0e1f84051c
unkosan <ryota.nakano@hamadalab.com>
2021-11-05 18:38:19 +0900

    changed all_rounds.py

--- scripts/all_rounds.py
+++ scripts/all_rounds.py
@@ -9,7 +9,7 @@ from pathlib import Path
 import torch
 
 from raptgen.models import CNN_PHMM_VAE
-from raptgen.data import SingleRound, Result
+from raptgen.data import SingleRound, Result, read_fastq
 from sklearn.mixture import GaussianMixture
 
 import os 
@@ -20,10 +20,11 @@ default_path = str(Path(f"{dir_path}/../out/gmm").resolve())
                     context_settings=dict(show_default=True))
 @click.argument("all-round-path",
                     type=click.Path(exists = True))
-@click.argument("seq-path",
-                    type=click.Path(exists = True))
 @click.argument("model-path",
                     type=click.Path(exists = True))
+@click.option("--seq-path", "-s",
+                    type=click.Path(exists = True),
+                    multiple=True)
 @click.option("--use-cuda/--no-cuda",
                   help    = "use cuda if available",
                   is_flag = True,
@@ -51,7 +52,15 @@ default_path = str(Path(f"{dir_path}/../out/gmm").resolve())
                   help    = "the number of class the Gaussian mixture model has",
                   type    = int,
                   default = 10)
-def main(all_round_path, seq_path, model_path, cuda_id, use_cuda, gmm_save_dir, latent_save_dir, fwd, rev, num_components):
+@click.option("--calc-times",
+                  help    = "the number of times calculation of GMM does (not num of iter EM)",
+                  type    = int,
+                  default = 100)
+@click.option("--min-count",
+                  help    = "minimal counts considered on all round GMM",
+                  type    = int,
+                  default = 1)
+def main(all_round_path, model_path, seq_path, cuda_id, use_cuda, gmm_save_dir, latent_save_dir, fwd, rev, num_components, calc_times, min_count):
     # get "all_rounds.py"-specific logger
     logger = logging.getLogger(__name__)
     
@@ -78,45 +87,56 @@ def main(all_round_path, seq_path, model_path, cuda_id, use_cuda, gmm_save_dir, 
     model.load_state_dict(torch.load(model_path, map_location=device))
 
     # get result instance of "all_rounds"
-    sround_result = Result(
+    all_round_result = Result(
         model,
         experiment           = experiment,
         path_to_save_results = gmm_save_dir,
-        load_if_exists       = True
+        load_if_exists       = True,
+        min_count            = min_count
     )
 
     # calc every mu vectors of probablistic function in latent space from the sequence data
     logger.info(f"calculating mean vectors of latent pdf defined by each SELEX reads")
-    sround_result.get_mean_vectors_from_experiment(get_raw_seq=False)
+    all_round_result.get_mean_vectors_from_experiment(get_raw_seq=False)
     if not Path(f"{latent_save_dir}/all_rounds_latent.csv").exists():
-        np.savetxt(f"{latent_save_dir}/all_rounds_latent.csv", sround_result.mus, delimiter=',')
+        np.savetxt(f"{latent_save_dir}/all_rounds_latent.csv", all_round_result.mus, delimiter=',')
     
-    # restore gmm model from "gmm.pkl", or calculate each var and mean of Gaussian mixuture.
-    # caution: if a gmm.pkl exists in "save_dir", discards the value of "num_class" and gets the gmm model from the gmm.pkl
-    sround_result.calc_gmm(dim = num_components)
+    all_round_result.calc_gmm(dim = num_components, calc_times=calc_times)
     logger.info(f"loading calculated GMM")
-    gmm_model: GaussianMixture = sround_result.gmm
+    gmm_model: GaussianMixture = all_round_result.gmm
 
     
     ## GMM MODEL, CNN-pHMM-VAE MODEL LOADED
     
 
-    if not ( Path(f"{latent_save_dir}/{Path(seq_path).stem}.csv").exists() 
-             and Path(f"{gmm_save_dir}/{Path(seq_path).stem}_probs.csv").exists() ):
-    # each probability of classes are predicted here
-        result = Result(
-            model,
-            experiment           = SingleRound( path            = seq_path,
-                                                forward_adapter = fwd,
-                                                reverse_adapter = rev ),
-            path_to_save_results = gmm_save_dir,
-            load_if_exists       = True
-        )
-        logger.info(f"predicting each probabilities of {num_components} classes for single round reads")
-        latent_seqs_mus_single_round = result.get_mean_vectors_from_experiment()
-        X_probs: np.ndarray = gmm_model.predict_proba(latent_seqs_mus_single_round)
-        np.savetxt(f"{latent_save_dir}/{Path(seq_path).stem}.csv", latent_seqs_mus_single_round, delimiter=',')
-        np.savetxt(f"{gmm_save_dir}/{Path(seq_path).stem}_probs.csv", X_probs, delimiter=',')
+    for single_round_path in seq_path:
+        if not ( Path(f"{latent_save_dir}/{Path(single_round_path).stem}.csv").exists() 
+                and Path(f"{gmm_save_dir}/{Path(single_round_path).stem}_probs.csv").exists() ):
+
+            # each probability of classes are predicted here
+            single_round_result = Result(
+                model,
+                experiment           = SingleRound(
+                    path            = single_round_path,
+                    forward_adapter = all_round_result.experiment.forward_adapter,
+                    reverse_adapter = all_round_result.experiment.reverse_adapter
+                ),
+                path_to_save_results = gmm_save_dir,
+                load_if_exists       = True,
+                min_count            = 1
+            )
+
+            single_round_mus, single_round_seqs = single_round_result.get_mean_vectors_from_experiment(get_raw_seq=True)
+            _, all_round_seqs = all_round_result.get_mean_vectors_from_experiment(get_raw_seq=True)
+
+            boolean_map = [seq in all_round_seqs for seq in single_round_seqs]
+            latent_seqs_mus_single_round = single_round_mus[boolean_map]
+
+            logger.info(f"predicting each probabilities of {num_components} classes for single round reads")
+            
+            X_probs: np.ndarray = gmm_model.predict_proba(latent_seqs_mus_single_round)
+            np.savetxt(f"{latent_save_dir}/{Path(single_round_path).stem}.csv", latent_seqs_mus_single_round, delimiter=',')
+            np.savetxt(f"{gmm_save_dir}/{Path(single_round_path).stem}_probs.csv", X_probs, delimiter=',')
     
     logger.info(f"finished")
 
